b781b324f0e4ffecf7edc01c68c7ad6be3b04102
mishamyrt <misha@myrt.co>
2022-03-21 11:35:16 +0400

    Fix buffer size

--- light_controller/src/dap/config.h
+++ light_controller/src/dap/config.h
@@ -1,4 +1,8 @@
 #pragma once
-#define INPUT_TIMEOUT 50
-#define INPUT_BUFFER_SIZE 128
+
+#define INPUT_DATA_SIZE 128
+
+/// 4 bytes for technical values. 2 for constants, 1 for length and one for checksum.
+#define INPUT_BUFFER_SIZE INPUT_DATA_SIZE + 4
 #define BOUD_RATE 2000000
+#define INPUT_TIMEOUT 50
