b99df499365d6e874f1b8c89198349bb63b749b6
mishamyrt <misha@myrt.co>
2022-03-21 14:25:19 +0400

    Improve remote flash script

--- scripts/flash-remote.sh
+++ scripts/flash-remote.sh
@@ -1,12 +1,28 @@
 #!/bin/sh
-DEVICE_PATH=/dev/ttyUSB0
-DIST_FOLDER=dist
-FQBN=arduino:avr:nano:cpu=atmega328old
 
-ssh lightstrip "rm -rf $DIST_FOLDER"
-scp -r "$DIST_FOLDER" "lightstrip:$DIST_FOLDER"
-ssh lightstrip "
+# User defined constants
+SSH_HOST="lightstrip"
+DEVICE_PATH="/dev/ttyUSB0"
+FIRMWARE_PATH="./dist/light_controller.ino.with_bootloader.hex"
+FQBN="arduino:avr:nano:cpu=atmega328old"
+
+# Internal constant
+FIRMWARE_NAME="$(basename $FIRMWARE_PATH)"
+
+# Action
+echo "Creating temporary directory"
+TEMP_DIR="$(ssh $SSH_HOST mktemp -d)"
+REMOTE_FIRMWARE_PATH="$TEMP_DIR/$FIRMWARE_NAME"
+echo "Sending firmware"
+scp "$FIRMWARE_PATH" "$SSH_HOST:$REMOTE_FIRMWARE_PATH"
+ssh $SSH_HOST "
+echo 'Stopping server'
 sudo service light-server stop
-arduino-cli upload --input-dir $DIST_FOLDER -p $DEVICE_PATH --fqbn $FQBN
+echo 'Flashing firmware'
+arduino-cli upload -t -i $REMOTE_FIRMWARE_PATH -p $DEVICE_PATH -b $FQBN
+echo 'Starting server'
 sudo service light-server start
+echo 'Cleaning up'
+rm -rf $TEMP_DIR
+echo 'Done ☺️'
 "
