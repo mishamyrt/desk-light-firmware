56b96db756064664a043584f1ac01213e1267c95
mishamyrt <misha@myrt.co>
2022-03-21 22:20:48 +0400

    Make rainbow great

--- light_controller/src/animator/effects/rainbow.h
+++ light_controller/src/animator/effects/rainbow.h
@@ -8,19 +8,29 @@
 class RainbowEffect: public Effect {
 public:
   void draw () {
-    hue = hue + 5;
-    if (step >= LED_COUNT) {
-      step = 0;
+    first_color.hue = getHue(0);
+    second_color.hue = getHue(60);
+    third_color.hue = getHue(120);
+
+    fill_gradient(
+      Light.leds,
+      LED_CENTER,
+      first_color,
+      second_color,
+      third_color,
+      FORWARD_HUES
+    );
+
+    Light.mirrorLEDs();
+
+    global_hue++;
+    if (global_hue > 255) {
+      global_hue = 0;
     }
-    if (hue > 255) {
-      hue = 0;
-    }
-    Light.setLED(step, CHSV(hue, 255, 255));
-    step++;
   }
 
   void prepare () {
-    hue = 0;
+    global_hue = 0;
   }
 
   uint8_t stepCount() {
@@ -28,9 +38,21 @@ public:
   }
 
   uint8_t stepDelay() {
-    return 10;
+    return 40;
   }
 
 private:
+  uint8_t getHue(uint8_t hue_shift) {
+    hue = global_hue + hue_shift;
+    if (hue > 255) {
+      hue -= 255;
+    }
+    return hue;
+  }
+
+  CHSV first_color = CHSV(0, 255, 255);
+  CHSV second_color = CHSV(0, 255, 255);
+  CHSV third_color = CHSV(0, 255, 255);
+  int global_hue;
   int hue;
 };
